905a0f394ffa3a50d7f3b8e48d9e6105872d0faa
zargrig <zaraadevelop@gmail.com>
2020-11-02 19:11:18 +0400

    Framework event add-destroy

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,7 +1,26 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
+  static className = 'excel__formula';
+
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click']
+    });
+  }
+
   toHTML() {
-    return '<h1>Formula</h1>'
+    return `
+      <div class="info">fx</div>
+      <div class="input" contenteditable spellcheck="false"></div>`
+  }
+
+  onInput(event) {
+    console.log('onInput ', event)
+  }
+
+  onClick(event) {
+    console.log('onClick ', event)
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,39 @@
-export class DomListener {
+import {capitalize} from '@core/utils';
 
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error(
+            `Method ${method} not implement in ${this.name} Component`
+        )
+      }
+
+      // bind created new function wrapper. so when we try removeEvent
+      // reference the another method.saving the reference and everytime call
+      this[method] = this[method].bind(this);
+
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
